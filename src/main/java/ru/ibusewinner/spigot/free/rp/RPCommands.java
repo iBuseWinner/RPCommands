39ff473336b3a497b3d9a1b64e44f8d4afe8296c
iBuseWinner <44753872+iBuseWinner@users.noreply.github.com>
2019-09-01 15:46:56 +0300

    Блаблабла, релиз плагина, блаблабла

--- src/main/java/ru/ibusewinner/spigot/free/rp/RPCommands.java
+++ src/main/java/ru/ibusewinner/spigot/free/rp/RPCommands.java
@@ -1,17 +1,74 @@
 package ru.ibusewinner.spigot.free.rp;
 
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.IOException;
 
 public final class RPCommands extends JavaPlugin {
 
+    protected static FileConfiguration cfg;
+    private static RPCommands plugin;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        cfg = getConfig();
+        plugin = this;
+        loadCfg();
+        Bukkit.getPluginCommand("rpcommands").setExecutor(new RPCmd());
+        Bukkit.getPluginManager().registerEvents(new CommandsListener(), this);
+        TimerRP.timer();
     }
+
+    public String getStr(String path) {
+        return cfg.getString(path).replaceAll("&", "§")
+                .replaceAll("%prefix%", cfg.getString("messages.prefix").replaceAll("&", "§"));
+    }
+
+    public int getInt(String path) {
+        return cfg.getInt(path);
+    }
+
+    protected static RPCommands getInstance() { return plugin; }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
+
+    public FileConfiguration loadCfg() {
+        cfg = super.getConfig();
+        cfg.options().copyDefaults(true);
+        saveConfig();
+        System.out.println(getStr("messages.loadcfg"));
+        return cfg;
+    }
+
+    @Override
+    public void saveConfig() {
+        try {
+            cfg.save(new File(getDataFolder()+ "/config.yml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public FileConfiguration getConfig() {
+        return cfg;
+    }
+
+    public void reloadConfiguration() {
+        cfg = super.getConfig();
+
+        try {
+            cfg.load(new File(getDataFolder()+ "/config.yml"));
+            saveConfig();
+        } catch (Exception e) {
+            loadCfg();
+        }
+    }
+
 }
