0f3bc5ec3dd598c8c61233b43c567d22513941e5
iBuseWinner <44753872+iBuseWinner@users.noreply.github.com>
2019-09-01 12:53:28 +0300

    Initial commit

---
+++ src/main/java/ru/ibusewinner/spigot/free/rp/RPCommands.java
@@ -0,0 +1,17 @@
+package ru.ibusewinner.spigot.free.rp;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RPCommands extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
